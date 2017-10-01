8d95cc55be8ebde7d166a565cce37e4212af58c4
Coolone36 <michaeljlaritz@gmail.com>
2017-10-01 19:57:45 -0400

    Added reverse pipe to reverse order with new on top, fixed remove photo option

--- PhotoBucket/src/app/app.module.ts
+++ PhotoBucket/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ReversePipe,
   ],
   imports: [
     BrowserModule,
